33e9e6e6975d428732191c25ff639d4cf61af2fe
Aliaksandr_Hulida <alex.gulida@gmail.com>
2015-09-16 16:17:17 +0300

    correct solution

--- src/main/java/com/coursera/SuperTable.java
+++ src/main/java/com/coursera/SuperTable.java
@@ -19,7 +19,7 @@ import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.util.Bytes;
 
 public class SuperTable{
-
+	
    public static void main(String[] args) throws IOException {
 
       // Instantiate Configuration class
@@ -27,7 +27,8 @@ public class SuperTable{
       // Instaniate HBaseAdmin class
 	   HBaseAdmin admin = new HBaseAdmin(con);
       // Instantiate table descriptor class
-	   HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf("superpowers"));
+//	   HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf("superpowers"));
+	   HTableDescriptor tableDescriptor = new HTableDescriptor("powers");
 	   tableDescriptor.addFamily(new HColumnDescriptor("personal"));
 	   tableDescriptor.addFamily(new HColumnDescriptor("professional"));
       // Add column families to table descriptor
@@ -37,42 +38,46 @@ public class SuperTable{
 	   tableDescriptor.addFamily(new HColumnDescriptor("xp"));
       // Execute the table through admin
 	   admin.createTable(tableDescriptor);
-	   System.out.println(" Table created ");
+//	   System.out.println(" Table created ");
       // Instantiating HTable class
-	   HTable hTable = new HTable(con, "superpowers");
+	   HTable hTable = new HTable(con, "powers");
       // Repeat these steps as many times as necessary
 
 	      // Instantiating Put class
               // Hint: Accepts a row name
-	   Put p = new Put(Bytes.toBytes("row1"));
+	  
       	      // Add values using add() method
               // Hints: Accepts column family name, qualifier/row name ,value
-	   p.add(Bytes.toBytes("personal"),Bytes.toBytes("hero"),Bytes.toBytes("superman"));
-	   p.add(Bytes.toBytes("personal"),Bytes.toBytes("hero"),Bytes.toBytes("batman"));
-	   p.add(Bytes.toBytes("personal"),Bytes.toBytes("hero"),Bytes.toBytes("volverine"));
 	   
 	   
-	   p.add(Bytes.toBytes("personal"),Bytes.toBytes("powers"),Bytes.toBytes("strength"));
-	   p.add(Bytes.toBytes("personal"),Bytes.toBytes("powers"),Bytes.toBytes("money"));
-	   p.add(Bytes.toBytes("personal"),Bytes.toBytes("powers"),Bytes.toBytes("healing"));
+	   String[][] matrix ={
+			       {"superman","strength","clark","100"},
+				   {"batman","money","bruce","50"},
+				   {"wolverine","healing","logan","75"}
+	   };
 	   
-	   p.add(Bytes.toBytes("personal"),Bytes.toBytes("name"),Bytes.toBytes("clark"));
-	   p.add(Bytes.toBytes("personal"),Bytes.toBytes("name"),Bytes.toBytes("bruce"));
-	   p.add(Bytes.toBytes("personal"),Bytes.toBytes("name"),Bytes.toBytes("logan"));
-	   
-	   
-	   p.add(Bytes.toBytes("personal"),Bytes.toBytes("xp"),Bytes.toBytes("100"));
-	   p.add(Bytes.toBytes("personal"),Bytes.toBytes("xp"),Bytes.toBytes("50"));
-	   p.add(Bytes.toBytes("personal"),Bytes.toBytes("xp"),Bytes.toBytes("75"));
-	   
-	   
+	   int i = 1;
+	  
+	   for (String row[] : matrix){
+		   		
+	
+		   	 	Put p = new Put(Bytes.toBytes("row"+i));
+	   		 	p.add(Bytes.toBytes("personal"),Bytes.toBytes("hero"),Bytes.toBytes(row[0]));
+			   	p.add(Bytes.toBytes("personal"),Bytes.toBytes("power"),Bytes.toBytes(row[1]));
+			   	p.add(Bytes.toBytes("professional"),Bytes.toBytes("name"),Bytes.toBytes(row[2]));
+			   	p.add(Bytes.toBytes("professional"),Bytes.toBytes("xp"),Bytes.toBytes(row[3]));
+			   	hTable.put(p);
+			   	i++;
+		   
+	   }
+
       // Save the table
-	   hTable.put(p);
-	   System.out.println("data inserted");
+//	   hTable.put(p);
+//	   System.out.println("data inserted");
       // Close table
 	   hTable.close();
       // Instantiate the Scan class
-	   HTable hTable2 = new HTable(con, "superpowers");
+	   HTable hTable2 = new HTable(con, "powers");
 	   Scan scan = new Scan();
       // Scan the required columns
 	   scan.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("hero"));
@@ -83,7 +88,7 @@ public class SuperTable{
       // Read values from scan result
       // Print scan result
 	   for (Result result = scanner.next(); result != null; result = scanner.next())
-	    System.out.println("Found row : " + result);
+	    System.out.println(result);
       // Close the scanner
    scanner.close();
       // Htable closer
